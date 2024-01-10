b7343d0a988c1b8cf1f9bc195dc7288db563f881
MarkusHN <145451050+MarkusHN@users.noreply.github.com>
2024-01-10 12:48:29 +0100

    main

---
+++ Java/Julekort.js
@@ -0,0 +1,18 @@
+function genererJulekort() {
+    let generator = document.getElementById ("julekort-generator");
+    generator.style.display="none"
+
+    let card = document.getElementById ("julekort");
+    card.style.display="block"
+
+    var julehilsen = document.getElementById("julehilsen-input").value;
+
+    var from = document.getElementById("from-input").value
+
+    var julekortDiv = document.getElementById("julekort");
+    julekortDiv.innerHTML = `
+        <h2 id='GJ'>God Jul!</h2>
+        <p id='helsing'>${julehilsen}</p>
+        <h3 id='from'>${from}</h3>
+    `;
+}
